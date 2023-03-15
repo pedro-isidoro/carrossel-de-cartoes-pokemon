292672cc63241993753b45ab81685c9443acb2f4
Pedro Isidoro <pedro.isidoro26@yahoo.com.br>
2023-03-15 10:34:22 -0300

    Atualizando.

--- src/js/index.js
+++ src/js/index.js
@@ -12,21 +12,30 @@
     - Passo 4 - buscar o cartão que esta selecionado e esconder
  */
 
-// - Passo 1 - Dar um jeito de pegar o elemento HTML da seta avançar;
-// document.getElementById('btn-avancar')
 const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
 const cartoes = document.querySelectorAll(".card");
 let cartaoAtual = 0;
 
-// - Passo 2 - Dar um jeito de identificar o clique do usuário na seta avançar;
+// Objetivo 1
 btnAvancar.addEventListener('click', () =>{
     // Executa até 2 ser identico á 2, depois para
     if(cartaoAtual === cartoes.length - 1) return
 
-    cartaoAtual++;
-    cartoes[cartaoAtual].classList.add("selected")
-
-    // - Passo 4 - buscar o cartão que esta selecionado e esconder;
     const cartaoSelecionado = document.querySelector(".selected");
     cartaoSelecionado.classList.remove("selected");
+
+    cartaoAtual++;
+    cartoes[cartaoAtual].classList.add("selected");
+});
+
+// Objetivo 2
+btnVoltar.addEventListener('click', () =>{
+    if(cartaoAtual === 0) return;
+
+    const cartaoSelecionado = document.querySelector(".selected");
+    cartaoSelecionado.classList.remove("selected");
+
+    cartaoAtual--;
+    cartoes[cartaoAtual].classList.add("selected")
 });
