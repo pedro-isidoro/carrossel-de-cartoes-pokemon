0c1c8e898e021fdde981b4c722247fcf3efa32a2
Pedro Isidoro <pedro.isidoro26@yahoo.com.br>
2023-03-15 10:25:51 -0300

    Atualizando.

--- src/js/index.js
+++ src/js/index.js
@@ -15,15 +15,18 @@
 // - Passo 1 - Dar um jeito de pegar o elemento HTML da seta avançar;
 // document.getElementById('btn-avancar')
 const btnAvancar = document.getElementById('btn-avancar');
+const cartoes = document.querySelectorAll(".card");
 let cartaoAtual = 0;
 
 // - Passo 2 - Dar um jeito de identificar o clique do usuário na seta avançar;
-btnAvancar.addEventListener('click', function(){
-    // - Passo 3 - Fazer aparecer o próximo cartão da lista;
-    const cartoes = document.querySelectorAll(".card");
+btnAvancar.addEventListener('click', () =>{
+    // Executa até 2 ser identico á 2, depois para
+    if(cartaoAtual === cartoes.length - 1) return
 
     cartaoAtual++;
     cartoes[cartaoAtual].classList.add("selected")
-});
 
-// - Passo 4 - buscar o cartão que esta selecionado e esconder;
+    // - Passo 4 - buscar o cartão que esta selecionado e esconder;
+    const cartaoSelecionado = document.querySelector(".selected");
+    cartaoSelecionado.classList.remove("selected");
+});
